0ce237084d6b4f7cf3ba3f202e98b838894272f4
= <=>
2016-03-05 16:01:41 -0500

    Add a little build script for Android and a necessary dependency zipalign

---
+++ androidRelease.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+./gradlew android:assembleRelease && 
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ./doremi-release-key.keystore ./android/build/outputs/apk/android-release-unsigned.apk doremi &&
+jarsigner -verify -verbose -certs ./android/build/outputs/apk/android-release-unsigned.apk &&
+./zipalign -v 4 ./android/build/outputs/apk/android-release-unaligned.apk ./android/build/outputs/apk/doremi.apk
+
+
