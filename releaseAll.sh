f4b8b88a16e950ec504614576dda1f713aa37d2c
= <=>
2016-03-13 13:55:26 -0400

    Add some additional release files

---
+++ releaseAll.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Releasing desktop..." && sh ./releaseDesktop.sh && 
+echo "Releasing Android..." && sh ./releaseAndroid.sh
--- releaseAndroid.sh
+++ releaseAndroid.sh
@@ -3,6 +3,6 @@
 ./gradlew android:assembleRelease && 
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ./doremi-release-key.keystore ./android/build/outputs/apk/android-release-unsigned.apk doremi &&
 jarsigner -verify -verbose -certs ./android/build/outputs/apk/android-release-unsigned.apk &&
-./zipalign -v 4 ./android/build/outputs/apk/android-release-unaligned.apk ./android/build/outputs/apk/doremi.apk
+./zipalign -v -f 4 ./android/build/outputs/apk/android-release-unaligned.apk ./android/build/outputs/apk/doremi.apk
 
 
